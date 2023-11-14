6ca068927006c2da2c94dde2d7c41c593a0ead1d
Nave-0 <144241939+Nave-0@users.noreply.github.com>
2023-11-14 18:24:27 -0500

    Update sweep.py

--- sweep.py
+++ sweep.py
@@ -5,8 +5,8 @@
 
   # Check whether the centre block is a bomb, a number, or an invalid input
   # Skip bombs, send an error on invalid input, verify numbers
-def validate( block_data ):
-  return validation
+
+  return 
 
 grid = [
   [0,0,0],
@@ -18,30 +18,5 @@ row = 0
 col = 0 
 bombs = 0
 
-while row < 3:
-    while col < 3: 
-#        print("here")
-        value = grid [row][col]
-#        print(row)
-#        print(col)
-#        print(value)
-        if value == -1:
-            bombs = bombs + 1
-            col = col + 1
-#            print(col)
-        else:
-            col = col + 1
-    row = row + 1
-    col = 0    
-#print(f"the number of bombs are {bombs}")
 
-middle_value = grid [1][1]
-#print(f"The middle value is {middle_value}")
 
-if middle_value == bombs:
-#    print(f"VALID")
-    validation = "VALID"
-else:
-#    print(f"INVALID CHECK INTERROIR BLOCK")
-    validation = "INVALID CHECK INTEROIR BLOCK"
-print (validate(grid))
